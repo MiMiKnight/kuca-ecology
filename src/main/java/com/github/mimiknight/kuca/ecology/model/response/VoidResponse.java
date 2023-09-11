4d0b19ddbe2d5798949e25c8267617708985a646
MiMiKnight <victor2015yhm@gmail.com>
2023-09-12 07:42:20 +0800

    【关联单号】US202308182020 【提交描述】新增VoidResponse

---
+++ src/main/java/com/github/mimiknight/kuca/ecology/model/response/VoidResponse.java
@@ -0,0 +1,10 @@
+package com.github.mimiknight.kuca.ecology.model.response;
+
+/**
+ * 空响应
+ *
+ * @author MiMiKnight victor2015yhm@gmail.com
+ * @since 2023-09-12 07:41:06
+ */
+public final class VoidResponse implements EcologyResponse {
+}
