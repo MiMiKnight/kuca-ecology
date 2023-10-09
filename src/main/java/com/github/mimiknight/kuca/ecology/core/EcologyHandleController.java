14b035218fe211031e7f906c1675a65d95f009ae
MiMiKnight <victor2015yhm@gmail.com>
2023-10-10 07:27:13 +0800

    【关联单号】US202308182020 【提交描述】新增handleAndGetBody方法

--- src/main/java/com/github/mimiknight/kuca/ecology/core/EcologyHandleController.java
+++ src/main/java/com/github/mimiknight/kuca/ecology/core/EcologyHandleController.java
@@ -2,6 +2,7 @@ package com.github.mimiknight.kuca.ecology.core;
 
 import com.github.mimiknight.kuca.ecology.handler.EcologyRequestHandler;
 import com.github.mimiknight.kuca.ecology.model.request.EcologyRequest;
+import com.github.mimiknight.kuca.ecology.model.response.BaseResponse;
 import com.github.mimiknight.kuca.ecology.model.response.EcologyResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,6 +58,33 @@ public abstract class EcologyHandleController {
     public <Q extends EcologyRequest, P extends EcologyResponse> P handle(Q request) throws Exception {
         Assert.notNull(request, "The request argument is required; it must not be null");
         return handlerExecutor.execute(request);
+    }
+
+    /**
+     * 处理方法
+     * <p>
+     * 仅限于继承自{@link BaseResponse}的响应
+     *
+     * @param <Q>           接口入参泛型
+     * @param <P>           接口出参泛型
+     * @param <B>           接口出参Body泛型
+     * @param request       接口入参
+     * @param responseClass 响应Class类型
+     * @return {@link B}
+     * @throws Exception 异常
+     */
+    public <B, Q extends EcologyRequest, P extends BaseResponse<B>> B handleAndGetBody(Q request, Class<P> responseClass) throws Exception {
+        Assert.notNull(request, "The request argument is required; it must not be null");
+
+        EcologyResponse eResponse = handlerExecutor.execute(request);
+
+        if (!(eResponse instanceof BaseResponse)) {
+            throw new IllegalStateException("Response is not inherited from BaseResponse.");
+        }
+
+        P response = responseClass.cast(eResponse);
+
+        return response.getBody();
     }
 
 }
